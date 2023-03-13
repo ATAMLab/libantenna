6cc052e3a906d887d480e49df69b34e440ba5753
Hüseyin <iletisim@huseyinyigit.com.tr>
2023-03-13 20:06:07 +0300

    proper sub project paths

--- release.ps1
+++ release.ps1
@@ -1,5 +1,5 @@
 # Use this Powershell script to push releases
-$version = "v1.1.1" 
-$commit  = "libconsts as a cmake subproject" 
+$version = "v1.2.0" 
+$commit  = "proper sub project paths" 
 Write-Output $version > version.tag
 git add . && git commit -m "$commit" && git push origin && gh release create $version --title "$version" --notes "$commit"
