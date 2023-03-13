e2ff29e9f17e4abe447ccb654e9ca37610bfb51d
Hüseyin <iletisim@huseyinyigit.com.tr>
2023-03-13 20:09:45 +0300

    double quotes for paths

--- release.ps1
+++ release.ps1
@@ -1,5 +1,5 @@
 # Use this Powershell script to push releases
-$version = "v1.2.0" 
-$commit  = "proper sub project paths" 
+$version = "v1.2.1" 
+$commit  = "double quotes for paths" 
 Write-Output $version > version.tag
 git add . && git commit -m "$commit" && git push origin && gh release create $version --title "$version" --notes "$commit"
