2a919a824632583a1158a859b08e581374a0d773
Hüseyin YİĞİT <iletisim@huseyinyigit.com.tr>
2023-03-13 14:54:04 +0300

    echo to version tag

--- release.ps1
+++ release.ps1
@@ -1,4 +1,5 @@
 # Use this Powershell script to push releases
 $version = "v1.0.1" 
-$commit  = "cmake fix" 
+$commit  = "upadate version" 
+Write-Output $version > version.tag
 git add . && git commit -m "$commit" && git push origin && gh release create $version --title "$version" --notes "$commit"
