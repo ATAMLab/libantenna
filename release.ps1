a6d24717edcd79213180054f25e6abc3d5191c95
Hüseyin <iletisim@huseyinyigit.com.tr>
2023-03-13 20:21:41 +0300

    cmake path convention

--- release.ps1
+++ release.ps1
@@ -1,5 +1,5 @@
 # Use this Powershell script to push releases
-$version = "v1.2.2" 
-$commit  = "double quotes for paths" 
+$version = "v1.2.3" 
+$commit  = "cmake path convention" 
 Write-Output $version > version.tag
 git add . && git commit -m "$commit" && git push origin && gh release create $version --title "$version" --notes "$commit"
