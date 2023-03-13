a81bd3945608315cc257cbe58bfed077f9750f9f
Hüseyin YİĞİT <iletisim@huseyinyigit.com.tr>
2023-03-13 14:38:31 +0300

    cmake fix

--- release.ps1
+++ release.ps1
@@ -1,4 +1,4 @@
 # Use this Powershell script to push releases
-$version = "v1.0.0" 
-$commit  = "initial stable release" 
+$version = "v1.0.1" 
+$commit  = "cmake fix" 
 git add . && git commit -m "$commit" && git push origin && gh release create $version --title "$version" --notes "$commit"
