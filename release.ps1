add41abdbfaed6ccaf5c9ab58dcbc7fc54874723
Hüseyin YİĞİT <iletisim@huseyinyigit.com.tr>
2023-03-20 10:24:30 +0300

    libconst dependency updated

--- release.ps1
+++ release.ps1
@@ -1,5 +1,5 @@
 # Use this Powershell script to push releases
-$version = "v1.5.0" 
-$commit  = "argument order revised" 
+$version = "v1.5.1" 
+$commit  = "libconst dependency updated" 
 Write-Output $version > version.tag
 git add . && git commit -m "$commit" && git push origin && gh release create $version --title "$version" --notes "$commit"
