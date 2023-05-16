b2c098cb4957d1c8c609b2f9de049968d5f45077
Hüseyin YİĞİT <yigit.hsyn@gmail.com>
2023-05-16 09:30:45 +0300

    add comments and test unix builds

--- antenna.h
+++ antenna.h
@@ -29,16 +29,26 @@ ANT_API double ant_wavelen2freq(double wavelen);
 ANT_API double ant_gamma2swr(double s11);
 ANT_API double ant_swr2gamma(double swr);
 
+/**
+ * Antenna far-field (Fraunhofer) starting range
+ * @param D antenna maxiumum cross-section size
+ * @param freq frequency of interest
+ * @return Range start for far-field
+ */
 ANT_API double ant_ffdist(double D, double freq);
+
+/**
+ * Antenna radiating near-field (Fresnel) starting range
+ * @param D antenna maxiumum cross-section size
+ * @param freq frequency of interest
+ * @return Range start for propagating near-field
+ */
 ANT_API double ant_nfdist(double D, double freq);
 
 ANT_API double ant_meas_nf_sepdist(double freq);
 ANT_API size_t ant_meas_nf_pnf_sspace(double freq);
 ANT_API size_t ant_meas_nf_pnf_scount(double freq, double L);
 ANT_API double ant_meas_nf_pnf_slength(double freq, double L);
-
-
-
 
 #ifdef __cplusplus
 }
